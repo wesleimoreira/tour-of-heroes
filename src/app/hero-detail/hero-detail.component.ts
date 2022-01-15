5d02c642ce51574feff0b4b9c22094f1d2cf1744
wesleimoreira <wesleimoreira600@gmail.com>
2022-01-14 21:09:20 -0300

    construindo a rota de detalhes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: 'hero-detail.component.html',
   styleUrls: ['hero-detail.component.scss'],
 })
-export class HeroDetailComponent {
- @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private heroservice: HeroService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroservice.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
